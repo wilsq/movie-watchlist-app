48abbcb4463e6c80534b1f40af5b33807644f50d
wilsq <viljami.j.viinikainen@gmail.com>
2025-12-16 15:33:24 +0200

    cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://movie-watchlist.com",
+      "https://www.movie-watchlist.com",
+    ],
+
     credentials: true,
   })
 );
