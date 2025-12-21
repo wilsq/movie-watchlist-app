d545f7551db5777a99fed37319f758d4b82faf31
Viljami V. <viljami.j.viinikainen@gmail.com>
2025-12-21 08:51:21 +0200

    Changed pool's ssl settings

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -12,7 +12,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false },
+  ssl: process.env.NODE_ENV === "test" ? false : { rejectUnauthorized: false },
 });
 
 export async function query(text: string, params?: any[]) {
